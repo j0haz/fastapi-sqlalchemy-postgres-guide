1b047964dcba257e427a5397702a310c16b755ef
j0haz <j0haz@users.noreply.github.com>
2025-11-09 23:00:18 +0100

    made changes to support sqlite database instead of postgres

--- app/src/model/db_manager.py
+++ app/src/model/db_manager.py
@@ -1,12 +1,9 @@
 import os
 import sqlalchemy as sqla
 
-POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB_PORT = os.getenv("POSTGRES_DB_PORT")
+DATABASE_FILE = os.getenv("SQLITE_DB")
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}"
+DATABASE_URL = f"sqlite:////app/databases/{DATABASE_FILE}"
 
 engine = sqla.create_engine(DATABASE_URL)
 
